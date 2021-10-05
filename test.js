8d946aba8337b00003fa236a1d82a5ba6a68d4d2
AZARIA <allysaidi64@gmail.com>
2021-10-05 21:52:54 +0200

    Add test code

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
+const calculator = {
+    add : (value1, value2) =>{
+        return value1+value2;
+    },
+    subtract: (value1, value2) =>{
+        return value1 - value2;
+    },
+    divide: (value1, value2) =>{
+        if( value2 ===0) return 0;
+        return value1/value2;
+    },
+    multiply: (value1, value2) =>{
+        return value1 * value2;
+    },
+}
+const capitalize = (value) =>{
+    value = [...value];
+    value[0]=value[0].toUpperCase();
+    value =value.join('');
+    return value;
+}
 const {stringLength, reverseString} = require('./chanllenge');
-// coreverseString = require('./chanllenge');
 
 test("Give the length of a string", ()=>{
     expect(stringLength("à")).toBeLessThanOrEqual(10);
@@ -16,4 +36,21 @@ test("Give the length of an empty string", ()=>{
 test("testing the function reverseString ...", ()=>{
     expect(reverseString('azy')).toEqual('yza');
 })
+describe('test object calculator', ()=>{
+    test('testing addition',()=>{
+        expect(calculator.add(2,4)).toEqual(6)
+    });
+    test('testing substracting',()=>{
+        expect(calculator.subtract(2,4)).toEqual(-2)
+    });
+    test('testing dividing',()=>{
+        expect(calculator.divide(2,4)).toBeCloseTo(0.5)
+    });
+    test('testing multiplying',()=>{
+        expect(calculator.multiply(2,4)).toEqual(8)
+    });
+});
+test("capitalize return the string in capitalize ", ()=>{
+    expect(capitalize('je suis là')).toEqual("Je suis là")
+});
 
